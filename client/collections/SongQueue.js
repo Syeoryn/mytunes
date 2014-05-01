ecdce402a844e9578232d3b49b89d96001696e74
Hack Reactor Students <syeoryn@gmail.com>
2014-04-30 19:48:04 -0700

    Adds playFirst function to SongQueue.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -1,7 +1,13 @@
 // SongQueue.js - Defines a backbone model class for the song queue.
 var SongQueue = Songs.extend({
 
-  initialize: function(){
+  initialize: function(songData){
+
+  },
+
+  playFirst: function() {
+    this.at(0).play();
   }
+
 
 });
